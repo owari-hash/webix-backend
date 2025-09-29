f876a4c9c020a4162e2e2e9b618a95b897c4796c
Anzai <55052716+owari-hash@users.noreply.github.com>
2025-09-29 23:57:06 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Simple route
+app.get('/', (req, res) => {
+  res.send('Hello from Express.js!');
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
